0464a8c3678e9dfc4d77b30a9d3400a82febc721
gouravmallick27 <84186459+GouravMallick@users.noreply.github.com>
2024-12-13 18:36:23 +0530

    Update BinarySearch.java
    updating the while condition

--- BinarySearch.java
+++ BinarySearch.java
@@ -12,7 +12,7 @@ public class BinarySearch {
         int start = 0;
         int end = arr.length - 1;
 
-        while (start < end) {
+        while (start <= end) {
             int mid = start + (end - start)/ 2;
 
             if(target < arr[mid]){
